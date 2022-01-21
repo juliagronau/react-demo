b57116d9db6679c4e962bdd0c109fc83a69bbdc1
julia <77528734+juliagronau@users.noreply.github.com>
2022-01-21 11:28:39 +0100

    refresher zu hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((data) => setArticles(data))
+        .catch((err) => console.log(err));
+    };
+    fetchData();
+  }, []);
+
+  console.log(articles);
+
   return (
     <div>
       <h1>Los geht's</h1>
+      {articles
+        ? articles.map((article) => (
+            <>
+              <h2>{article.title}</h2>
+              <p>{article.body}</p>
+            </>
+          ))
+        : "Loading..."}
     </div>
   );
 }
